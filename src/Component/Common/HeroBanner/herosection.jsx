5396657f5c623ef717ff26e04b4d94cd94d50384
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2025-05-07 17:57:48 +0530

    working

--- src/Component/Common/HeroBanner/herosection.jsx
+++ src/Component/Common/HeroBanner/herosection.jsx
@@ -14,14 +14,14 @@ const Herosection = () => {
             Milky Way Galaxy
           </h3>
         </div>
-        {/* <ul className="d-flex justify-content-between mb-0 mt-4 pt-2 px-0">
+        <ul className="d-flex justify-content-between mb-0 mt-4 pt-2 px-0">
             <li className="list-unstyled text-dark-blue fw-semibold font-size-24"><span className="d-block font-size-16 fw-normal text-light-white mb-1">Meteors Gained</span>300 Meteors</li>
             <li className="list-unstyled text-dark-blue fw-semibold font-size-24"><span className="d-block font-size-16 fw-normal text-light-white mb-1">Stars Gained</span>0 Stars</li>
             <li className="list-unstyled fw-normal font-size-46 text-white">Planet A</li>
             <li className="list-unstyled text-dark-blue fw-semibold font-size-24"><span className="d-block font-size-16 fw-normal text-light-white mb-1">Total Meteors</span>1000 Meteors</li>
             <li className="list-unstyled text-dark-blue fw-semibold font-size-24"><span className="d-block font-size-16 fw-normal text-light-white mb-1">Planet Status</span>Unlocked</li>
-        </ul> */}
-        <div className="row justify-content-center mt-5 pt-5">
+        </ul>
+        {/* <div className="row justify-content-center mt-5 pt-5">
           <div className="col-3 col-lg-3 ">
           <div className="list-unstyled text-dark-blue fw-semibold font-size-24"><span className="d-block font-size-16 fw-normal text-light-white mb-1">Meteors Gained</span>300 Meteors</div>
           <div className="list-unstyled text-dark-blue fw-semibold font-size-24"><span className="d-block font-size-16 fw-normal text-light-white mb-1">Stars Gained</span>0 Stars</div>
@@ -33,7 +33,7 @@ const Herosection = () => {
             <div className="list-unstyled text-dark-blue fw-semibold font-size-24"><span className="d-block font-size-16 fw-normal text-light-white mb-1">Total Meteors</span>1000 Meteors</div>
             <div className="list-unstyled text-dark-blue fw-semibold font-size-24"><span className="d-block font-size-16 fw-normal text-light-white mb-1">Planet Status</span>Unlocked</div>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
