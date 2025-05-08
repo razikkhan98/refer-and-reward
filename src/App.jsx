1101f2bad308e8705139d975f0a3a7b49c22f555
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2025-05-08 11:58:44 +0530

    remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Home from './Component/Pages/Home/home';
+import SecondScreen from './Component/Pages/secondScreen/secondScreen';
 function App() {
   return (
     <>
       <Home />
+      {/* <SecondScreen/> */}
     </>
   );
 }
