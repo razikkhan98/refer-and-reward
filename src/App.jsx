e58fdeea32a310714816d1623ca8692758810684
pragatijoshi-ui <pragati.joshi@skillintelligence.tech>
2025-05-08 12:18:48 +0530

    invite-friend component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '../src/styles/main.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 // import '@fontsource/space-grotesk';
 // import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Home from './Component/Pages/Home/home';
+
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration (ms)
+      once: false,     // Whether animation should happen only once
+    });
+  }, []);
   const [count, setCount] = useState(0)
 
   return (
@@ -18,4 +28,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- src/Component/Pages/Home/home.jsx
+++ src/Component/Pages/Home/home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import Herosection from '../../Common/HeroBanner/herosection'
-import Navbar from '../../Common/Navbar/navbar'
-import PlanetSlider from '../../Common/PlanetSlider/planetSlider'
+import Herosection from '../../Common/HeroBanner/herosection';
+import Navbar from '../../Common/Navbar/navbar';
+import PlanetSlider from '../../Common/PlanetSlider/planetSlider';
+import Invitefriend from '../../Common/Invitefriend/invitefriend';
 
 const Home = () => {
   return (
+    <>
     <section className='hero-section  position-relative' id=''>
         <Navbar/>
         <Herosection/>
         <PlanetSlider/>
     </section>
+     <Invitefriend/>
+     </>
+    
   )
 }
 
